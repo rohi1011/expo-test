63a3d69746f793328fb6afb9d41efbdcea8b9390
Rohith <rrohith1011@gmail.com>
2024-02-09 17:56:11 +0530

    Build Trigger

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -3,3 +3,7 @@ import { addNumbers } from "../src/mathUtils";
 test('add numbers', ()=> {
    expect(addNumbers(1,2)).toBe(3); 
 });
+
+test('subtract numbers', ()=> {
+   expect(addNumbers(3,2)).toBe(1); 
+});
